10df903ff45db4a78955862da127a9b359f5e262
Mark Wylde <129744061+markwylde-deriv@users.noreply.github.com>
2023-04-27 14:15:12 +0100

    Initial commit

---
+++ build.js
@@ -0,0 +1,35 @@
+import esbuild from 'esbuild';
+import { promises as fs } from 'fs';
+import chokidar from 'chokidar';
+import debounce from 'debounce';
+import { createRequire } from 'node:module';
+
+const require = createRequire(import.meta.url);
+
+async function build() {
+  esbuild.buildSync({
+    bundle: true,
+    entryPoints: ['./src/index.tsx'],
+    outdir: 'dist',
+    loader: {
+      '.ts': 'ts',
+      '.tsx': 'tsx',
+    },
+    define: {
+      'process.env.NODE_ENV': '"' + process.env.NODE_ENV + '"'
+    },
+    sourcemap: true
+  });
+
+  await Promise.all([
+    fs.cp('src/index.html', 'dist/index.html'),
+    fs.cp(require.resolve('@deriv-experiments/react/dist/worker.js'), 'dist/worker.js')
+  ]);
+}
+
+if (process.argv[2] === '--watch' || process.argv[2] === '-w') {
+  chokidar.watch('src/**/*', { ignoreInitial: false }).on('all', debounce(build, 100));
+  console.log('watching for changes');
+} else {
+  build();
+}
---
+++ src/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import Deriv, { useAuthorize } from '@deriv-experiments/react'
+
+const Dashboard = () => {
+  const authorize = useAuthorize();
+
+  return (
+    <div>
+      <h1>Dashboard</h1>
+
+      {authorize
+        ? (
+          <>
+            Logged in as {' '}
+            <strong>{authorize.email}</strong>
+          </> 
+        )
+        : (
+          <button onClick={Deriv.login}>Login</button>
+        )
+      }
+
+      <ul>
+        <li>Dashboard</li>
+        <li><a href="/example1/">Apps</a></li>
+        <li><a href="/example2/">Statement</a></li>
+        <li><a href="/debug/">Debug</a></li>
+      </ul>
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById('app'));
+
+root.render(
+  <Dashboard />
+);
